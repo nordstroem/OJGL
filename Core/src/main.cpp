86a5f0eeb900b8c3c034e08dde75466a5c90069a
Johan Nordström <cjs.nordstrom@gmail.com>
2017-07-25 22:47:09 +0200

    Properly load gl functions

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -9,13 +9,14 @@ palette). */
 #include <GL/gl.h> /* OpenGL header file */
 #include <GL/glu.h> /* OpenGL utilities header file */
 #include "gl_loader.h"
+#include <iostream>
 
 // clang-format on
 
 void display()
 {
     /* rotate a triangle around */
-	glActiveTexture(0);
+	glActiveTexture(GL_TEXTURE0);
 	glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_TRIANGLES);
     glColor3f(1.0f, 0.0f, 0.0f);
@@ -148,6 +149,7 @@ int main()
     hDC = GetDC(hWnd);
     hRC = wglCreateContext(hDC);
     wglMakeCurrent(hDC, hRC);
+	load_gl_functions();
 
     ShowWindow(hWnd, 1);
 
