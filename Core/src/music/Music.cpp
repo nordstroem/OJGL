ac0270f2a40d769dafedfaec3bf010d0c86f5c92
Johan Nordström <cjs.nordstrom@gmail.com>
2017-08-05 17:04:22 +0200

    Correct case on filenames

---
+++ Core/src/music/Music.cpp
@@ -0,0 +1,73 @@
+#include "Music.h"
+#include "render/Window.h"
+#include <iostream>
+#include <map>
+#include <set>
+
+namespace ojgl {
+
+Music::Music(unsigned char* song)
+{
+    this->_player = std::make_unique<V2MPlayer>();
+    this->_song = song;
+}
+
+Music::~Music()
+{
+    this->_player->Close();
+}
+
+void Music::play()
+{
+    this->_player->Init();
+    this->_player->Open(this->_song);
+    this->_player->Play();
+    while (this->_player->IsPlaying())
+        this->_player->Tick();
+    this->_player->Stop();
+    initSync();
+    dsInit(this->_player->RenderProxy, this->_player.get(), GetForegroundWindow());
+    this->_player->Play();
+}
+
+void Music::initSync()
+{
+    const int MIN = -1;
+    const int MAX = 999999;
+    std::map<int, int> channelMinNote;
+    std::map<int, int> channelMaxNote;
+    std::set<int> channels;
+    for (auto& se : _player->popSyncEvents()) {
+        int channel = se.channel;
+        int note = se.note;
+
+        channels.insert(channel);
+
+        if (channelMinNote.find(channel) == channelMinNote.end())
+            channelMinNote[channel] = MAX;
+        if (channelMaxNote.find(channel) == channelMaxNote.end())
+            channelMaxNote[channel] = MIN;
+
+        channelMinNote[channel] = min(channelMinNote[channel], note);
+        channelMaxNote[channel] = max(channelMaxNote[channel], note);
+    }
+
+    for (auto& c : channels) {
+        int numNotes = channelMaxNote[c] - channelMinNote[c] + 1;
+        std::cout << "Channel " << c << " with " << numNotes << " notes\n";
+        syncChannels[c] = SyncChannel(numNotes, channelMinNote[c], c);
+    }
+}
+
+void Music::updateSync()
+{
+    for (auto& se : this->_player->popSyncEvents()) {
+        syncChannels[se.channel].setVelocity(se.note, (double)se.velocity);
+    }
+
+    for (auto& kv : syncChannels) {
+        kv.second.tick();
+    }
+}
+
+} //namespace ojgl
---
+++ Core/src/music/SyncChannel.cpp
@@ -0,0 +1,65 @@
+#include "SyncChannel.h"
+#include <stdexcept>
+
+SyncChannel::SyncChannel()
+    : _decay(-1.0)
+    , _minNote(-1)
+    , channel(-1)
+    , numNotes(-1)
+{
+    //throw std::logic_error("This constructor should not be called");
+}
+
+SyncChannel::SyncChannel(int numNotes, int minNote, int channel, double decay)
+    : _decay(decay)
+    , _minNote(minNote)
+    , channel(channel)
+    , numNotes(numNotes)
+{
+    _currentNoteVelocities.resize(numNotes);
+    _totalHitsPerNote.resize(numNotes);
+}
+
+SyncChannel::~SyncChannel()
+{
+}
+
+void SyncChannel::tick()
+{
+    for (auto& velocity : _currentNoteVelocities)
+        velocity *= 0.95f;
+}
+
+void SyncChannel::setVelocity(int absoluteNote, double velocity)
+{
+    if (velocity > 1.0) {
+        _totalHitsPerNote[absoluteNote - _minNote]++;
+        _currentNoteVelocities[absoluteNote - _minNote] = velocity;
+    }
+}
+
+double SyncChannel::getNoteVelocity(int relativeNote)
+{
+    return _currentNoteVelocities[relativeNote];
+}
+
+double SyncChannel::getTotalNoteVelocity()
+{
+    double sum = 0.0;
+    for (auto& velocity : _currentNoteVelocities)
+        sum += velocity;
+    return sum;
+}
+
+int SyncChannel::getTotalHitsPerNote(int relativeNote)
+{
+    return _totalHitsPerNote[relativeNote];
+}
+
+int SyncChannel::getTotalHits()
+{
+    int sum = 0;
+    for (auto& amount : _totalHitsPerNote)
+        sum += amount;
+    return sum;
+}
