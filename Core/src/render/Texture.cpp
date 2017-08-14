86d015ae23d44495bcd15c0ae4a811652f1475bc
Johan Nordström <cjs.nordstrom@gmail.com>
2017-08-14 18:42:55 +0200

    Removed debug mode error and started on texture

---
+++ Core/src/render/Texture.cpp
@@ -0,0 +1,28 @@
+#include "Texture.h"
+#include "winapi\gl_loader.h"
+
+namespace ojgl {
+
+Texture::Texture()
+{
+}
+
+Texture::~Texture()
+{
+}
+
+void Texture::load(unsigned char* img)
+{
+    glGenTextures(1, &_textureID);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, _textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+} //namespace ojgl
