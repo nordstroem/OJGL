7495747d94f3bb6c0d5f457a774ae873498ed8c2
Johan Nordström <cjs.nordstrom@gmail.com>
2018-07-21 16:19:07 +0200

    Changed title

--- Core/src/render/Window.cpp
+++ Core/src/render/Window.cpp
@@ -102,7 +102,7 @@ HWND Window::CreateOpenGLWindow(const char* title, int x, int y, BYTE type, DWOR
         }
     }
 
-    hWnd = CreateWindow(L"OpenGL", L"Hej", WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+    hWnd = CreateWindow(L"OpenGL", L"Eldur - OJ", WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
         x, y, this->_width, this->_height, NULL, NULL, hInstance, NULL);
     if (fullScreen) {
         hWnd = CreateFullscreenWindow(hWnd, hInstance);
