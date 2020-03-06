37ce26b62a63c0e8c25f37499a3d28cc2f2a7b38
Johan Nordström <cjs.nordstrom@gmail.com>
2020-03-06 09:57:31 +0100

    Use raymarchBase.fs.

--- ojgl/examples/Main.cpp
+++ ojgl/examples/Main.cpp
@@ -14,90 +14,21 @@ using namespace ojgl;
 void buildSceneGraph(GLState& glState, int x, int y)
 {
     glState.clearScenes();
-
     {
-        auto geometry = Buffer::construct(x, y, "geometry", "shaders/edison.vs", "shaders/cachedGeometry.fs", {}, ojgl::BufferFormat::Quad, true);
-        auto lightning = Buffer::construct(x, y, "lightning", "shaders/edison.vs", "shaders/lightning.fs", { geometry });
-        glState.addScene("cachedGeometryScene", lightning, Duration::seconds(100));
-    }
-    {
-        //auto edison = Buffer::construct(BufferFormat::Quad, x, y, "intro", "shaders/edison.vs", "shaders/lavaIntro.fs");
-        //auto fxaa = Buffer::construct(BufferFormat::Quad, x, y, "fxaa", "shaders/fxaa.vs", "shaders/fxaa.fs", edison);
-        //auto post = Buffer::construct(BufferFormat::Quad, x, y, "post", "shaders/post.vs", "shaders/post.fs", fxaa);
-
-        auto mesh = Buffer::construct(x, y, "mesh", "shaders/mesh.vs", "shaders/mesh.fs", {}, BufferFormat::Meshes);
-
-        glState.addScene("meshScene", mesh, Duration::seconds(2));
-    }
-    {
-        auto noise = Buffer::construct(x, y, "intro", "shaders/demo.vs", "shaders/mountainNoise.fs");
-        auto mountain = Buffer::construct(x, y, "fxaa", "shaders/demo.vs", "shaders/mountain.fs", { noise });
-        auto fxaa = Buffer::construct(x, y, "fxaa", "shaders/fxaa.vs", "shaders/fxaa.fs", { mountain });
-        auto post = Buffer::construct(x, y, "post", "shaders/demo.vs", "shaders/mountainPost.fs", { fxaa });
-        glState.addScene("introScene", post, Duration::seconds(77));
-    }
-
-    {
-        auto edison = Buffer::construct(x, y, "intro", "shaders/edison.vs", "shaders/lavaScene2.fs");
-        auto fxaa = Buffer::construct(x, y, "fxaa", "shaders/fxaa.vs", "shaders/fxaa.fs", { edison });
-        auto post = Buffer::construct(x, y, "post", "shaders/post.vs", "shaders/post.fs", { fxaa });
-        glState.addScene("introScene", post, Duration::seconds(40));
-    }
-    {
-        auto edison = Buffer::construct(x, y, "intro", "shaders/edison.vs", "shaders/outro.fs");
-        auto fxaa = Buffer::construct(x, y, "fxaa", "shaders/fxaa.vs", "shaders/fxaa.fs", { edison });
-        auto post = Buffer::construct(x, y, "post", "shaders/post.vs", "shaders/post.fs", { fxaa });
-        glState.addScene("introScene", post, Duration::seconds(40));
+        auto buffer = Buffer::construct(x, y, "template", "demo.vs", "raymarchBase.fs", {}, ojgl::BufferFormat::Quad);
+        glState.addScene("raymarch scene", buffer, Duration::seconds(100));
     }
 }
 
 int main(int argc, char* argv[])
 {
-    auto popupData = popup::show();
-
-    OJ_UNUSED(argc);
-    OJ_UNUSED(argv);
-    int width = popupData.width;
-    int height = popupData.height;
-    bool fullScreen = popupData.full;
+    int width = 1280;
+    int height = 720;
+    bool fullScreen = false;
     bool showCursor = !fullScreen;
-
-    /*#ifndef _DEBUG
-    fullScreen = true;
-    showMouse = false;
-	#endif
-    if (argc >= 3) {
-        width = std::stoi(argv[1]);
-        height = std::stoi(argv[2]);
-        if (argc >= 4) {
-            full = static_cast<bool>(std::stoi(argv[3]));
-        }
-    }
-    */
-
-    ShaderReader::setBasePath("examples/");
-    ShaderReader::preLoad("shaders/edison.vs", resources::vertex::edison);
-    ShaderReader::preLoad("shaders/demo.vs", resources::vertex::demo);
-    ShaderReader::preLoad("shaders/post.vs", resources::vertex::post);
-    ShaderReader::preLoad("shaders/fxaa.vs", resources::vertex::fxaa);
-    ShaderReader::preLoad("shaders/fxaa.fs", resources::fragment::fxaa);
-    ShaderReader::preLoad("shaders/post.fs", resources::fragment::post);
-    ShaderReader::preLoad("shaders/lavaIntro.fs", resources::fragment::lavaIntro);
-    ShaderReader::preLoad("shaders/mountain.fs", resources::fragment::mountain);
-    ShaderReader::preLoad("shaders/mountainNoise.fs", resources::fragment::mountainNoise);
-    ShaderReader::preLoad("shaders/mountainPost.fs", resources::fragment::mountainPost);
-    ShaderReader::preLoad("shaders/lavaScene2.fs", resources::fragment::lavaScene2);
-    ShaderReader::preLoad("shaders/outro.fs", resources::fragment::outro);
-    ShaderReader::preLoad("shaders/mesh.vs", resources::vertex::mesh);
-    ShaderReader::preLoad("shaders/mesh.fs", resources::fragment::mesh);
-    ShaderReader::preLoad("shaders/cachedGeometry.fs", resources::fragment::cachedGeometry);
-    ShaderReader::preLoad("shaders/lightning.fs", resources::fragment::lightning);
-
-    // @todo move this into GLState? We can return a const reference to window.
-    // and perhaps have a unified update() which does getMessages(), music sync update and
-    // so on.
-    Window window(width, height, "Eldur - OJ", fullScreen, showCursor);
-    GLState glState(resources::songs::song);
+    ShaderReader::setBasePath("examples/shaders/");
+    Window window(width, height, "Raymarch template", fullScreen, showCursor);
+    GLState glState;
     buildSceneGraph(glState, width, height);
     glState.initialize();
 
@@ -116,23 +47,18 @@ int main(int argc, char* argv[])
             case Window::KEY_LEFT:
                 glState.changeTime(Duration::milliseconds(-5000));
                 break;
-
             case Window::KEY_RIGHT:
                 glState.changeTime(Duration::milliseconds(5000));
                 break;
-
             case Window::KEY_SPACE:
                 glState.togglePause();
                 break;
-
             case Window::KEY_R:
                 glState.restart();
                 break;
-
             case Window::KEY_UP:
                 glState.nextScene();
                 break;
-
             case Window::KEY_DOWN:
                 glState.previousScene();
                 break;
@@ -140,14 +66,8 @@ int main(int argc, char* argv[])
             }
         }
 
-        glState["meshScene"]["mesh"].insertMesh(mesh, Matrix::scaling(0.2f) * Matrix::rotation(1, 1, 1, glState.relativeSceneTime().toSeconds()));
-        //glState["meshScene"]["mesh"].insertMesh(mesh, Matrix::scaling(0.4f) * Matrix::translation(0.3, ojstd::sin(glState.relativeSceneTime().toSeconds()), 0.0));
-
-        // TODO: Aspect ratio
-        glState << UniformMatrix4fv("P", Matrix::perspective(45.0f * 3.14159265f / 180.0f, 16.0f / 9.0f, 0.001f, 1000.0f) * Matrix::translation(0.0, 0.0, -5.0));
-
         glState << Uniform1f("iTime", glState.relativeSceneTime().toSeconds());
-        glState << Uniform1f("iGlobalTime", glState.relativeSceneTime().toSeconds() - 2.f);
+        glState << Uniform1f("iGlobalTime", glState.relativeSceneTime().toSeconds());
         glState << Uniform2f("iResolution", static_cast<float>(width), static_cast<float>(height));
         glState.update();
 
