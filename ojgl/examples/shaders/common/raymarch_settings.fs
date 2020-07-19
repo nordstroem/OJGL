372484cbb90c6fe4aec7a5019624d71f208f4ff2
Oskar Pedersen <oskar.pedersen1@gmail.com>
2020-07-19 16:15:32 +0200

    Distance multiplier

--- ojgl/examples/shaders/common/raymarch_settings.fs
+++ ojgl/examples/shaders/common/raymarch_settings.fs
@@ -1,6 +1,7 @@
 R""(
-const float distanceEpsilon = 2e-2;
-const float normalEpsilon = 1e-2;
-const int maxSteps = 100;
-const float maxDistance = 400.0;
+const float S_distanceEpsilon = 2e-2;
+const float S_normalEpsilon = 1e-2;
+const int S_maxSteps = 100;
+const float S_maxDistance = 400.0;
+const float S_distanceMultiplier = 1.0;
 )""
--- ojgl/examples/shaders/common/raymarch_utils.fs
+++ ojgl/examples/shaders/common/raymarch_utils.fs
@@ -16,7 +16,7 @@ DistanceInfo map(in vec3 p);
 
 vec3 normal(in vec3 p)
 {
-    vec3 n = vec3(map(vec3(p.x + normalEpsilon, p.y, p.z)).distance, map(vec3(p.x, p.y + normalEpsilon, p.z)).distance, map(vec3(p.x, p.y, p.z + normalEpsilon)).distance);
+    vec3 n = vec3(map(vec3(p.x + S_normalEpsilon, p.y, p.z)).distance, map(vec3(p.x, p.y + S_normalEpsilon, p.z)).distance, map(vec3(p.x, p.y, p.z + S_normalEpsilon)).distance);
     return normalize(n - map(p).distance);
 }
 
@@ -26,13 +26,13 @@ MarchResult march(in vec3 rayOrigin, in vec3 rayDirection)
 {
     float t = 0.0;
     MarchResult invalidResult = { invalidType, vec3(0.0), 0 };
-    for (int steps = 0; steps < maxSteps; ++steps) {
+    for (int steps = 0; steps < S_maxSteps; ++steps) {
         vec3 p = rayOrigin + t * rayDirection;
         DistanceInfo info = map(p);
-        t += info.distance;
-        if (info.distance < distanceEpsilon)
+        t += info.distance * S_distanceMultiplier;
+        if (info.distance < S_distanceEpsilon)
             return MarchResult(info.type, p, steps);
-        if (t > maxDistance)
+        if (t > S_maxDistance)
             return invalidResult;
     }
     return invalidResult;
