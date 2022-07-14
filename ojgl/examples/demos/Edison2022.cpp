e2dc2f6bf603878da0c16a4a025ff7630eb4f7f6
Johan Nordström <nordstroem@gmail.com>
2022-07-14 23:27:15 +0200

    End last scene

--- ojgl/examples/demos/Edison2022.cpp
+++ ojgl/examples/demos/Edison2022.cpp
@@ -254,7 +254,7 @@ ojstd::vector<Scene> Edison2022::buildSceneGraph(const Vector2i& sceneSize) cons
             return vector;
         });
 
-        scenes.emplace_back(chrom, Duration::seconds(999999), "scene3");
+        scenes.emplace_back(chrom, Duration::seconds(32), "scene3");
     }
     return scenes;
 }
