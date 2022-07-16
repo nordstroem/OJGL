7fcd153feefb83d18fa7b2b0c4a9c079663f0ab3
Johan Nordström <nordstroem@gmail.com>
2022-07-16 20:28:13 +0200

    Remove unused stuff

--- ojgl/examples/EmbeddedResources.h
+++ ojgl/examples/EmbeddedResources.h
@@ -31,227 +31,19 @@ namespace ojgl::resources {
           },
 
           {
-  #include "shaders/common/raymarch_template.fs"
-          , "common/raymarch_template.fs"
-          },
-
-          {
-  #include "shaders/eldur/post.fs"
-          , "eldur/post.fs"
-          },
-
-          {
   #include "shaders/common/fxaa.fs"
           , "common/fxaa.fs"
           },
-
-          {
-  #include "shaders/eldur/lavaIntro.fs"
-          , "eldur/lavaIntro.fs"
-          },
-
-          {
-  #include "shaders/eldur/mountain.fs"
-          , "eldur/mountain.fs"
-          },
-
-          {
-  #include "shaders/eldur/mountainNoise.fs"
-          , "eldur/mountainNoise.fs"
-          },
-
-          {
-  #include "shaders/eldur/mountainPost.fs"
-          , "eldur/mountainPost.fs"
-          },
-
-          {
-  #include "shaders/eldur/lavaScene2.fs"
-          , "eldur/lavaScene2.fs"
-          },
-
-          {
-  #include "shaders/eldur/outro.fs"
-          , "eldur/outro.fs"
-          },
-
-          {
-  #include "shaders/experiments/cachedGeometry.fs"
-          , "experiments/cachedGeometry.fs"
-          },
-
-          {
-  #include "shaders/experiments/lightning.fs"
-          , "experiments/lightning.fs"
-          },
-
-          {
-  #include "shaders/common/fxaa.vs"
+          
+{
+  #include "shaders/common/fxaa.fs"
           , "common/fxaa.vs"
           },
-
           {
   #include "shaders/common/quad.vs"
           , "common/quad.vs"
           },
 
-          {
-#include "shaders/common/radial_blur.fs"
-          , "common/radial_blur.fs"
-          },
-          {
-  #include "shaders/common/fade.fs"
-          , "common/fade.fs"
-          },
-          {
-#include "shaders/QED/raymarch_utils.fs"
-            , "QED/raymarch_utils.fs"
-          },
-
-          {
-#include "shaders/QED/raymarch_utils_tower.fs"
-          , "QED/raymarch_utils_tower.fs"
-          },
-
-          {
-#include "shaders/QED/blur1.fs"
-          , "QED/blur1.fs"
-          },
-
-          {
-#include "shaders/QED/post.fs"
-          , "QED/post.fs"
-          },
-
-          {
-#include "shaders/QED/rayMarch.fs"
-          , "QED/rayMarch.fs"
-          },
-
-          {
-#include "shaders/QED/rayMarch.vs"
-          , "QED/rayMarch.vs"
-          },
-
-          {
-#include "shaders/QED/sphere.fs"
-          , "QED/sphere.fs"
-          },
-
-          {
-#include "shaders/QED/sphere.vs"
-          , "QED/sphere.vs"
-          },
-
-          {
-#include "shaders/QED/tower.fs"
-          , "QED/tower.fs"
-          },
-
-          {
-#include "shaders/dodenstriumf/graveScene.fs"
-          , "dodenstriumf/graveScene.fs"
-          },
-
-          {
-#include "shaders/dodenstriumf/graveScenePost.fs"
-          , "dodenstriumf/graveScenePost.fs"
-          },
-
-          {
-#include "shaders/dodenstriumf/introScene.fs"
-          , "dodenstriumf/introScene.fs"
-          },
-
-          {
-#include "shaders/dodenstriumf/post.fs"
-          , "dodenstriumf/post.fs"
-          },
-
-          {
-#include "shaders/dodenstriumf/roomScene.fs"
-          , "dodenstriumf/roomScene.fs"
-          },
-
-          {
-#include "shaders/dodenstriumf/roomScenePost.fs"
-          , "dodenstriumf/roomScenePost.fs"
-          },
-
-          {
-#include "shaders/innersystemlab/edison_1.fs"
-            , "innersystemlab/edison_1.fs"
-          },
-
-          {
-#include "shaders/innersystemlab/edison_1_post.fs"
-            , "innersystemlab/edison_1_post.fs"
-          },
-
-          {
-#include "shaders/innersystemlab/edison_2.fs"
-            , "innersystemlab/edison_2.fs"
-          },
-
-          {
-#include "shaders/innersystemlab/edison_planets.fs"
-            , "innersystemlab/edison_planets.fs"
-          },
-
-          {
-#include "shaders/innersystemlab/hospital.fs"
-            , "innersystemlab/hospital.fs"
-          },
-                          {
-#include "shaders/edison2021/raymarch_settings.fs"
-            , "edison2021/raymarch_settings.fs"
-          },
-
-          {
-#include "shaders/edison2021/main.fs"
-            , "edison2021/main.fs"
-          },
-
-          {
-#include "shaders/edison2021/main2.fs"
-            , "edison2021/main2.fs"
-          },
-          {
-#include "shaders/edison2021/tunnel_base.fs"
-            , "edison2021/tunnel_base.fs"
-          },
-          {
-#include "shaders/edison2021/bird.fs"
-          , "edison2021/bird.fs"
-          },
-          {
-#include "shaders/edison2021/pipes.fs"
-          , "edison2021/pipes.fs"
-          },
-          {
-#include "shaders/edison2021/blob.fs"
-          , "edison2021/blob.fs"
-          },
-          {
-#include "shaders/edison2021/introText.fs"
-          , "edison2021/introText.fs"
-          },
-          {
-#include "shaders/edison2021/wire_cube.fs"
-          , "edison2021/wire_cube.fs"
-          },
-          {
-#include "shaders/edison2021/tunnel_transform.fs"
-          , "edison2021/tunnel_transform.fs"
-          },
-          {
-#include "shaders/edison2021/plane.fs"
-          , "edison2021/plane.fs"
-          },
-          {
-#include "shaders/edison2021/chrom_ab.fs"
-          , "edison2021/chrom_ab.fs"
-          },
           {
 #include "shaders/edison2022/raymarch_settings.fs"
           , "edison2022/raymarch_settings.fs"
@@ -261,24 +53,12 @@ namespace ojgl::resources {
           , "edison2022/raymarch_settings_refract.fs"
           },
           {
-#include "shaders/edison2022/experiment.fs"
-          , "edison2022/experiment.fs"
-          },
-          {
 #include "shaders/edison2022/chrom_ab.fs"
           , "edison2022/chrom_ab.fs"
-          },
-                        {
-#include "shaders/edison2022/blocks.fs"
-          , "edison2022/blocks.fs"
           },
           {
 #include "shaders/edison2022/grass.fs"
           , "edison2022/grass.fs"
-          },
-          {
-#include "shaders/edison2022/seed.fs"
-          , "edison2022/seed.fs"
           },
           {
 #include "shaders/edison2022/jellyfish.fs"
@@ -287,10 +67,6 @@ namespace ojgl::resources {
          {
 #include "shaders/edison2022/intro.fs"
           , "edison2022/intro.fs"
-          },
-          {
-#include "shaders/edison2022/intro.fs"
-          , "edison2022/post.fs"
           },
           {
 #include "shaders/edison2022/ascent.fs"
--- ojgl/examples/Main.cpp
+++ ojgl/examples/Main.cpp
@@ -2,13 +2,7 @@
 #include "FreeCameraController.h"
 #include "TextRenderer.hpp"
 #include "demo/Demo.h"
-#include "demos/DodensTriumf.h"
-#include "demos/Edison2021.h"
 #include "demos/Edison2022.h"
-#include "demos/Eldur.h"
-#include "demos/InnerSystemLab.h"
-#include "demos/QED.h"
-#include "demos/Template.h"
 #include "render/GLState.h"
 #include "render/Popup.h"
 #include "render/Texture.h"
@@ -39,26 +33,7 @@ enum class DemoType {
 
 ojstd::shared_ptr<Demo> getDemo(DemoType type)
 {
-    switch (type) {
-    case DemoType::Eldur:
-        return ojstd::make_shared<Eldur>();
-    case DemoType::QED:
-        return ojstd::make_shared<QED>();
-    case DemoType::DodensTriumf:
-        return ojstd::make_shared<DodensTriumf>();
-    case DemoType::InnerSystemLab:
-        return ojstd::make_shared<InnerSystemLab>();
-    case DemoType::Template:
-        return ojstd::make_shared<Template>();
-    case DemoType::Edison2021:
-        return ojstd::make_shared<Edison2021>();
-    case DemoType::Edison2022:
-        return ojstd::make_shared<Edison2022>();
-    }
-
-    _ASSERTE(false);
-
-    return nullptr;
+    return ojstd::make_shared<Edison2022>();
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
